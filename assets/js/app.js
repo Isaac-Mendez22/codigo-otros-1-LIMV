82d7a72b83773ac4d56e66ebb3b8f123e954f98e
Isaac22 <luisisaacmv22@gmail.com>
2025-05-22 15:06:09 -0600

    se añade ejercicio

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,23 @@
 const baseEndpoint = 'https://api.github.com';
 const usersEndpoint = `${baseEndpoint}/users`;
-const $n = document.querySelector('name');
+const $n = document.querySelector('#name'); //falta el #
 const $b = document.querySelector('#blog');
-const $l = document.querySelector('.location');
+const $l = document.querySelector('#location');
 
-function displayUser(username) {
+async function displayUser(username) {  //falta el async para que funcione el await al ser una promesa asincrona
   $n.textContent = 'cargando...';
   const response = await fetch(`${usersEndpoint}/${username}`);
+  const data = await response.json();  //falta traer el json
   console.log(data);
-  $n.textContent = '${data.name}';
-  $b.textContent = '${data.blog}';
-  $l.textContent = '${data.location}';
+  $n.textContent = `${data.name}`;
+  $b.textContent = `${data.blog}`;
+  $l.textContent = `${data.location}`; /*se cambian apostrofes por bactips */
 }
 
 function handleError(err) {
   console.log('OH NO!');
   console.log(err);
-  n.textContent = `Algo salió mal: ${err}`
+  $n.textContent = `Algo salió mal: ${err}` /* se agrega el $n que si esta declarado */
 }
 
 displayUser('stolinski').catch(handleError);
